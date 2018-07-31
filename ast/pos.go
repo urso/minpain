d31bad250c0116625b6b71082b2c8ae15a435b73
urso <steffen.siering@elastic.co>
2018-07-31 02:56:58 +0200

    Add (ast.Pos).String()

--- ast/pos.go
+++ ast/pos.go
@@ -1,6 +1,15 @@
 package ast
 
+import "fmt"
+
 type Pos struct {
 	Source       string
 	Line, Column int
+}
+
+func (p Pos) String() string {
+	if p.Source == "" {
+		return fmt.Sprintf("%v:%v", p.Line, p.Column)
+	}
+	return fmt.Sprintf("%v:%v:%v", p.Source, p.Line, p.Column)
 }
