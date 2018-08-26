02cb9427a2ebbd45d9b3ca249b506803419ed055
urso <steffen.siering@elastic.co>
2018-08-27 00:59:23 +0200

    Introduce ssa.Unit type

--- compile/ssa/ssa.go
+++ compile/ssa/ssa.go
@@ -1,15 +1,26 @@
 package ssa
 
 import (
+	"io"
+
 	"github.com/urso/minpain/ast"
 	"github.com/urso/minpain/constant"
 	"github.com/urso/minpain/types"
 )
 
+type Unit interface {
+	genValueID() ID
+	genBlockID() ID
+}
+
 type Program struct {
 	Funcs map[string]*Func
 
-	Main *Func
+	Entry  *Block
+	Blocks []*Block
+
+	blockIDs idAlloc
+	valueIDs idAlloc
 }
 
 type Func struct {
@@ -27,7 +38,7 @@ type Func struct {
 
 type Block struct {
 	ID     ID
-	Parent *Func
+	Parent Unit
 	Pos    ast.Pos
 
 	Preds []Edge
@@ -43,6 +54,9 @@ type Edge struct {
 
 type Instruction interface {
 	instNode()
+	HasID() bool
+	GetID() ID
+	Format(io.Writer) error
 }
 
 type instr struct {
@@ -64,7 +78,7 @@ type Const struct { // constant value
 	instr
 
 	ID  ID
-	Pos Pos
+	Pos ast.Pos
 
 	Value constant.Value
 	Type  types.Type
@@ -72,20 +86,51 @@ type Const struct { // constant value
 
 func (_ *instr) instNode()     {}
 func (i *instr) Block() *Block { return i.block }
-func (i *instr) Parent() *Func { return i.block.Parent }
+func (i *instr) Parent() Unit  { return i.block.Parent }
+
+func (_ *Value) HasID() bool { return true }
+func (v *Value) GetID() ID   { return v.ID }
+
+func (_ *Const) HasID() bool { return true }
+func (c *Const) GetID() ID   { return c.ID }
+
+func (prog *Program) genBlockID() ID {
+	return prog.blockIDs.next()
+}
+
+func (prog *Program) genValueID() ID {
+	return prog.valueIDs.next()
+}
+
+func (prog *Program) newBlock() *Block {
+	blk := &Block{
+		ID:     prog.genBlockID(),
+		Parent: prog,
+	}
+	prog.Blocks = append(prog.Blocks, blk)
+	return blk
+}
 
 func (fn *Func) newBlock() *Block {
 	blk := &Block{
-		ID:     fn.blockIDs.next(),
+		ID:     fn.genBlockID(),
 		Parent: fn,
 	}
 	fn.Blocks = append(fn.Blocks, blk)
 	return blk
 }
 
+func (fn *Func) genBlockID() ID {
+	return fn.blockIDs.next()
+}
+
+func (fn *Func) genValueID() ID {
+	return fn.valueIDs.next()
+}
+
 func (b *Block) newValue(op Op, t types.Type, args ...Instruction) *Value {
 	val := &Value{
-		ID:   b.Parent.valueIDs.next(),
+		ID:   b.Parent.genValueID(),
 		Op:   op,
 		Type: t,
 		Args: args,
@@ -94,6 +139,18 @@ func (b *Block) newValue(op Op, t types.Type, args ...Instruction) *Value {
 	val.block = b
 	b.Instr = append(b.Instr, val)
 	return val
+}
+
+func (b *Block) newConst(t types.Type, val constant.Value) *Const {
+	c := &Const{
+		ID:    b.Parent.genValueID(),
+		Type:  t,
+		Value: val,
+	}
+
+	c.block = b
+	b.Instr = append(b.Instr, c)
+	return c
 }
 
 func (b *Block) NumPredecessors() int {
@@ -106,7 +163,7 @@ func (b *Block) Predecssor(i int) *Block {
 
 func (b *Block) newPhi(t types.Type, args ...Instruction) *Value {
 	val := &Value{
-		ID:   b.Parent.valueIDs.next(),
+		ID:   b.Parent.genValueID(),
 		Op:   OpPhi,
 		Type: t,
 		Args: args,
