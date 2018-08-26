4996b4d6a2690134a0eb360443137bf4facab199
urso <steffen.siering@elastic.co>
2018-08-27 00:57:59 +0200

    add constructors to contant package

--- constant/constant.go
+++ constant/constant.go
@@ -40,6 +40,7 @@ type strVal struct {
 func (_ *unknownVal) Kind() Kind     { return Unknown }
 func (_ *unknownVal) String() string { return "<unknown>" }
 
+func Boolean(b bool) Value    { return &boolVal{b} }
 func (v *boolVal) Kind() Kind { return Bool }
 func (v *boolVal) String() string {
 	if v.b {
@@ -48,11 +49,14 @@ func (v *boolVal) String() string {
 	return "false"
 }
 
+func Number(i int64) Value       { return &numVal{i} }
 func (v *numVal) Kind() Kind     { return Numeric }
 func (v *numVal) String() string { return fmt.Sprintf("%v", v.i) }
 
+func Float(f float64) Value      { return &decVal{f} }
 func (v *decVal) Kind() Kind     { return Decimal }
 func (v *decVal) String() string { return fmt.Sprintf("%v", v.f) }
 
+func Str(s string) Value         { return &strVal{s} }
 func (v *strVal) Kind() Kind     { return String }
-func (v *strVal) String() string { return fmt.Sprintf("%q", v.s) }
+func (v *strVal) String() string { return v.s }
